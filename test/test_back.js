f692f269d3ae6f235f8bb970ed24805e654154a3
mrichelmy <mathieu.richelmy@insa-lyon.fr>
2021-09-21 10:13:42 +0200

    add test part

---
+++ test/test_back.js
@@ -0,0 +1,19 @@
+var assert = require('assert');
+const products = require('../back/services/products');
+
+/**
+ * Services tests
+ * 
+ * 1st test concerns the GET request getProductsByCity that return products filtered by cities.
+ * Input : Array cityList
+ * Output expected : []
+ */
+describe('Test services that send requests on Products table', function() {
+  describe('Get products filtered by cities', function() {
+    var cityList = []
+    it('should return [] when the list of cities is empty', async function() {
+      const rows = await products.getProductsByCity(cityList);
+      assert.equal(rows.data.length, 0);
+    });
+  });
+});
