c3b6e9101fe8c70b9ad7db51da2bc99a28b1c02e
mrichelmy <mathieu.richelmy@insa-lyon.fr>
2021-09-21 12:50:11 +0200

    add doculentation to test_front

--- test/test_front.js
+++ test/test_front.js
@@ -1,6 +1,12 @@
 // var assert = require('assert');
 // const { addTag } = require('../front/scripts/product');
-
+/**
+ * Tag tests
+ * 
+ * 1st test verify that we are not taking into account a word which is not in the cities' list
+ * Input : String wordTested
+ * Output expected : false
+ */
 // describe('Test functions that modify dynamically the zone 2 (tag zone)', function() {
 //   describe('Test the addition of tag', function() {
 //     var wordTested = "arbre";
