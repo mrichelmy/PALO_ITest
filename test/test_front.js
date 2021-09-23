5d1ff5fde7667f0e8b52dc1d093e0b81c82b227a
mrichelmy <mathieu.richelmy@insa-lyon.fr>
2021-09-23 12:08:00 +0200

    replace front test

--- test/test_front.js
+++ test/test_front.js
@@ -1,18 +1,27 @@
 // var assert = require('assert');
-// const { addTag } = require('../front/scripts/product');
+// var calculateFromString = require('../front/scripts/product');
 /**
- * Tag tests
+ * Calculator tests
  * 
- * 1st test verify that we are not taking into account a word which is not in the cities' list
- * Input : String wordTested
- * Output expected : false
+ * 1st test verify all operators
+ * Input : two numbers, num1 and num2
+ * Output expected : the operations between num1 and num2
  */
-// describe('Test functions that modify dynamically the zone 2 (tag zone)', function() {
-//   describe('Test the addition of tag', function() {
-//     var wordTested = "arbre";
-    
-//     it('should return false if the word tested is not in the list of cities', function() {
-//       assert.equal(addTag(wordTested), false);
+// describe('Test function that calculate a result from string input', function() {
+//   describe('Test the operators', function() {   
+//     var num1 = 2;
+//     var num2 = 2; 
+//     it('should return addition of num1 and num2', function() {
+//       assert.equal(calculateFromString(`${num1} + ${num2}`), num1 + num2);
+//     });
+//     it('should return subtraction of num1 and num2', function() {
+//         assert.equal(calculateFromString(`${num1} - ${num2}`), num1 - num2);
+//     });
+//     it('should return multiplication of num1 and num2', function() {
+//         assert.equal(calculateFromString(`${num1} * ${num2}`), num1 * num2);
+//     });
+//     it('should return division of num1 and num2', function() {
+//         assert.equal(calculateFromString(`${num1} / ${num2}`), num1 / num2);
 //     });
 //   });
 // });
